1524f877badae613b5934a5b7b04db24095ca09b
GGedde <ggedde@users.noreply.github.com>
2020-05-22 15:54:25 -0700

    Create SpryRateLimits.php

---
+++ src/SpryRateLimits.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license MIT
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Spry\SpryProvider;
+
+use Spry\Spry;
+
+/**
+ * Rate Limit Provider for Spry
+ */
+class SpryRateLimits
+{
+    /**
+     * Constructor for Rate Limits
+     */
+    public static function initiate()
+    {
+    
+    }
+}
